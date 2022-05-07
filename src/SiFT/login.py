850a27979b7f5991a69a87b46b0bc9cd1de16c99
Kovács Gergely <gkovacs1@edu.bme.hu>
2022-05-07 20:02:15 +0200

    login request from bytes

--- src/SiFT/login.py
+++ src/SiFT/login.py
@@ -1,16 +1,23 @@
 import time
-from SiFT.mtp import MTP
+
+_encoding = "utf_8"
 
 
 class LoginRequest():
-    def __init__(self, uname: str, pw: str, rnd: bytes) -> None:
+    def __init__(self, uname: str, pw: str, rnd: bytes, ts=time.time_ns()) -> None:
         self.uname = uname
         self.pw = pw
         self.rnd = rnd
-        self.ts = time.time_ns()
+        self.ts = ts
 
     def get_request(self) -> bytes:
-        return bytes(f'{self.ts}\n{self.uname}\n{self.pw}\n{self.rnd}', MTP.encoding)
+        return bytes(f'{self.ts}\n{self.uname}\n{self.pw}\n', _encoding) + self.rnd
+
+    def from_bytes(data: bytes):
+        rnd = data[-16:]
+        tmp = data[0:-16].decode(_encoding).split('\n')
+        print(tmp)
+        return LoginRequest(tmp[1], tmp[2], rnd, int(tmp[0]))
 
 
 class LoginResponse():
