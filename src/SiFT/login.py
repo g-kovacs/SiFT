ae553b873ef7f9935a1a26afd3fb574a1c4f084b
Kovács Gergely <gkovacs1@edu.bme.hu>
2022-05-07 18:01:40 +0200

    refactor login request

--- src/SiFT/login.py
+++ src/SiFT/login.py
@@ -1,25 +1,18 @@
 import time
+from SiFT.mtp import MTP
 
 
-class LoginBase:
-    encoding = 'utf_8'
-
-    def __init__(self) -> None:
-        pass
-
-
-class LoginRequest(LoginBase):
+class LoginRequest():
     def __init__(self, uname: str, pw: str, rnd: bytes) -> None:
-        super().__init__()
         self.uname = uname
         self.pw = pw
         self.rnd = rnd
         self.ts = time.time_ns()
 
     def get_request(self) -> bytes:
-        return bytes(f'{self.ts}\n{self.uname}\n{self.pw}\n{self.rnd}', LoginBase.encoding)
+        return bytes(f'{self.ts}\n{self.uname}\n{self.pw}\n{self.rnd}', MTP.encoding)
 
 
-class LoginResponse(LoginBase):
+class LoginResponse():
     def __init__(self) -> None:
-        super().__init__()
+        pass
