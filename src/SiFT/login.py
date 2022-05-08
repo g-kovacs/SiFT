c6b28e88bba78d1827d19ecf37e54804e4fb4e57
Kovács Gergely <gkovacs1@edu.bme.hu>
2022-05-08 14:27:29 +0200

    fix login data (SHA256 hash problems)

--- src/SiFT/login.py
+++ src/SiFT/login.py
@@ -24,7 +24,7 @@ class Logins:
 
 
 class LoginRequest():
-    def __init__(self, uname: str, pw: str, rnd: bytes, ts: int) -> None:
+    def __init__(self, uname: str, pw: str, rnd: str, ts: int) -> None:
         self.uname = uname
         self.pw = pw
         self.rnd = rnd
@@ -35,14 +35,13 @@ class LoginRequest():
             return False
         return self.get_request() == __o.get_request()
 
-    def get_request(self) -> bytes:
-        return bytes(f'{self.ts}\n{self.uname}\n{self.pw}\n', _encoding) + self.rnd
+    def get_request(self) -> str:
+        return f'{self.ts}\n{self.uname}\n{self.pw}\n{self.rnd}'
 
     def from_bytes(data: bytes):
-        rnd = data[-16:]
-        tmp = data[0:-16].decode(_encoding).split('\n')
+        tmp = data.decode(_encoding).split('\n')
         print(tmp)
-        return LoginRequest(tmp[1], tmp[2], rnd, int(tmp[0]))
+        return LoginRequest(tmp[1], tmp[2], tmp[3], int(tmp[0]))
 
     def valid_timestamp(self, ts: int, delta_s: int):
         delta_ns = delta_s * int(5e8)
@@ -50,6 +49,6 @@ class LoginRequest():
 
 
 class LoginResponse():
-    def __init__(self, req: LoginRequest, rnd: bytes) -> None:
+    def __init__(self, req: LoginRequest, rnd: str) -> None:
         self.req = req
         self.rnd = rnd
