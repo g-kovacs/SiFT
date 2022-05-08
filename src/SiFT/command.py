4193b8e9b50454f36548097947d9b059c464d3ea
Kovács Gergely <gkovacs1@edu.bme.hu>
2022-05-08 12:21:59 +0200

    command example

--- src/SiFT/command.py
+++ src/SiFT/command.py
@@ -8,6 +8,16 @@ class Command:
         self.host = host
 
     def execute(self):
+        # feldolgozod a parancsot
+        # ...
+        # előállítod a subpacket-et
+        # pl. chd "chd\npelda_dir" --> bytes --> data
+        # data = bytes(2)
+        # mtp: MTPEntity = self.host.MTP
+        # mtp.send_message(MTP.COMMAND_REQ, data)
+        pass
+
+    def choose_cmd_type(self):
         pass
 
 
@@ -16,4 +26,5 @@ class CommandHandler:
         self.rootdir = dir
 
     def handle(self, cmd: bytes):
+
         pass
